44d93406c67d1bf0e9fe550f7fe75e634f25c3b1
DmitriyKim01 <120229930+DmitriyKim01@users.noreply.github.com>
2023-01-06 17:14:01 -0500

    Delete HashMaps.java

--- Data_Structures/HashMaps.java
+++
@@ -1,9 +0,0 @@
-package Data_Structures;
-
-import java.util.HashMap;
-
-public class HashMaps {
-    public static void main(String[] args) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-    }
-}
